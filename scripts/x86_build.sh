b6e180b80b46a4701833d21d4abf8e9350235708
SinterWong <sintercver@gmail.com>
2025-08-26 11:14:07 +0800

    update: complete x86_build script

--- scripts/x86_build.sh
+++ scripts/x86_build.sh
@@ -5,32 +5,34 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+REPO_DIR=$(pwd)
+
 # Define the build directory
-BUILD_DIR="build"
-INSTALL_DIR="install"
+BUILD_DIR="${REPO_DIR}/build"
+INSTALL_DIR="${REPO_DIR}/install"
 
 # Clean up previous build artifacts
-if [ -d "$BUILD_DIR" ]; then
-    echo "Cleaning up previous build directory: $BUILD_DIR"
-    rm -rf "$BUILD_DIR"
+if [ -d "${BUILD_DIR}" ]; then
+    echo "Cleaning up previous build directory: ${BUILD_DIR}"
+    rm -rf "${BUILD_DIR}"
 fi
 
-if [ -d "$INSTALL_DIR" ]; then
-    echo "Cleaning up previous install directory: $INSTALL_DIR"
-    rm -rf "$INSTALL_DIR"
+if [ -d "${INSTALL_DIR}" ]; then
+    echo "Cleaning up previous install directory: ${INSTALL_DIR}"
+    rm -rf "${INSTALL_DIR}"
 fi
 
 echo "Updating git submodules..."
 git submodule update --init --recursive
 
 echo "Creating 3rdparty target directory..."
-mkdir -p 3rdparty/target
+mkdir -p ${REPO_DIR}/3rdparty/target
 
 echo "Downloading dependency archive..."
 curl -L https://github.com/sinterwong/ai-core/releases/download/v1.0.0-alpha/dependency-Linux_x86_64.tgz -o dependency.tgz
 
 echo "Extracting dependency archive..."
-tar xvf dependency.tgz -C 3rdparty/target
+tar xvf dependency.tgz -C ${REPO_DIR}/3rdparty/target
 
 echo "Removing dependency archive..."
 rm dependency.tgz
@@ -41,28 +43,31 @@ sudo apt-get install -y gcc-13 g++-13
 sudo apt-get install -y ninja-build
 
 echo "Configuring CMake project..."
-cmake -B "$BUILD_DIR" -GNinja \
-    -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake -B "${BUILD_DIR}" -GNinja \
+    -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
     -DBUILD_AI_CORE_TESTS=ON \
     -DBUILD_AI_CORE_BENCHMARKS=ON \
     -DWITH_ORT_ENGINE=ON \
     -DWITH_NCNN_ENGINE=ON \
     -DWITH_TRT_ENGINE=OFF \
     -DCMAKE_BUILD_TYPE=Release \
-    -S ./
+    -S ${REPO_DIR}
 
 echo "Building project..."
-cmake --build "$BUILD_DIR" --config Release
+cmake --build "${BUILD_DIR}" --config Release
 
 echo "Installing project..."
-cmake --install "$BUILD_DIR"
+cmake --install "${BUILD_DIR}"
 
 echo "Setting up environment variables for tests..."
-export BUILD_3RD_DIR="$BUILD_DIR"/3rdparty
-export DL_3RD_DIR=3rdparty/target/Linux_x86_64
-export LD_LIBRARY_PATH="$INSTALL_DIR"/lib:"${BUILD_3RD_DIR}"/encryption-tool/x86_64/lib:"${BUILD_3RD_DIR}"/logger/x86_64/lib:"${DL_3RD_DIR}"/onnxruntime/lib:"${DL_3RD_DIR}"/opencv/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+export BUILD_3RD_DIR="${BUILD_DIR}/3rdparty"
+export DL_3RD_DIR="${REPO_DIR}/3rdparty/target/Linux_x86_64"
 
-cd "$INSTALL_DIR"
+export LD_LIBRARY_PATH="${INSTALL_DIR}/lib":"${BUILD_3RD_DIR}/encryption-tool/x86_64/lib":"${BUILD_3RD_DIR}/logger/x86_64/lib":"${DL_3RD_DIR}/onnxruntime/lib":"${DL_3RD_DIR}/opencv/lib"${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+
+echo $LD_LIBRARY_PATH
+
+cd "${INSTALL_DIR}"
 echo "Running tests..."
 ./tests/ai_core_tests --gtest_filter=*.*
 
