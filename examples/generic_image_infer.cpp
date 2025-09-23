b906d17f71e54ea14ff4b1c478fa7452ffd62f30
SinterWong <sintercver@gmail.com>
2025-09-23 14:24:47 +0800

    feat: add batchInfer for GenericImageInfer

--- examples/generic_image_infer.cpp
+++ examples/generic_image_infer.cpp
@@ -42,4 +42,44 @@ AlgoOutput GenericImageInfer::operator()(const cv::Mat &image,
 
   return algoOutput;
 }
+
+std::vector<ai_core::AlgoOutput>
+GenericImageInfer::operator()(const std::vector<cv::Mat> &images,
+                              const std::vector<cv::Rect> &rois) {
+  if (images.empty()) {
+    LOG_ERRORS << "Input images vector is empty";
+    return {};
+  }
+  if (images.size() != rois.size()) {
+    LOG_ERRORS << "Input images and rois vectors must have the same size";
+    return {};
+  }
+
+  std::vector<AlgoInput> algoInputs(images.size());
+  for (size_t i = 0; i < images.size(); ++i) {
+    if (images[i].empty()) {
+      LOG_WARNINGS << "Input image at index " << i << " is empty, skipping.";
+      continue;
+    }
+
+    cv::Rect detRoi = rois[i];
+    if (detRoi.empty()) {
+      detRoi = cv::Rect(0, 0, images[i].cols, images[i].rows);
+    }
+
+    FrameInput frameInput;
+    frameInput.image = std::make_shared<cv::Mat>(images[i]);
+    frameInput.inputRoi = std::make_shared<cv::Rect>(detRoi);
+    algoInputs[i].setParams(frameInput);
+  }
+
+  std::vector<AlgoOutput> algoOutputs;
+  if (mEngine->batchInfer(algoInputs, mParams.preprocParams,
+                          mParams.postprocParams,
+                          algoOutputs) != InferErrorCode::SUCCESS) {
+    LOG_ERRORS << "engine batch infer failed";
+    return {};
+  }
+  return algoOutputs;
+}
 } // namespace ai_core::example
